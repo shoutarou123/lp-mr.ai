e938954e0af7e5c49fba8bdd9f70764ba898049a
shoutarou123 <850123.a@gmail.com>
2025-09-19 22:29:41 +0900

    LP作成開始

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ミスターAI | 株式会社taiziii（タイジー） </title>
+  <meta name="description" content="">
+</head>
+
+<body>
+</body>
+
+</html>
