80ace6c1e474e862f3bdd35a07886a817e4fa2ec
shoutarou123 <850123.a@gmail.com>
2025-09-20 21:44:48 +0900

    FV完成

---
+++ js/hamburger-menu.js
@@ -0,0 +1,20 @@
+$(function () {
+  var $header = $('#header');
+  var $hamburgerCloseBtn = $('#hamburgerCloseBtn');
+  var $hamburgerOpenBtn = $('#hamburgerOpenBtn');
+  var $navigation = $('.navigation');
+
+  $hamburgerCloseBtn.click(function () {
+    $hamburgerCloseBtn.addClass('hidden');
+    $hamburgerOpenBtn.removeClass('hidden');
+    $header.css('background-color', '#FFF');
+    $navigation.slideToggle(300);
+  });
+
+  $hamburgerOpenBtn.click(function () {
+    $hamburgerOpenBtn.addClass('hidden');
+    $hamburgerCloseBtn.removeClass('hidden');
+    $header.css('background-color', '');
+    $navigation.slideToggle(300);
+  });
+});
